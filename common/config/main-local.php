3ea90f4ff7c4b5bfd4f466156cbf84adc0579c25
bendoe <badjornor@softcube.co>
2014-07-16 20:33:34 +0000

    Merge branch 'bencopy' of https://github.com/sirantho20/voiceout into bencopy

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=voiceout',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=voiceout',
             'username' => 'voiceout',
             'password' => 'voiceout',
             'charset' => 'utf8',
